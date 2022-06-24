280b0c8067f547c50772dd7f07ca3b9a857310fa
ShivJAVA <105239382+ShivJAVA@users.noreply.github.com>
2022-06-24 06:28:57 -0500

    Update GenieRunnerL2.java

--- GenieRunnerL2.java
+++ GenieRunnerL2.java
@@ -1,4 +1,12 @@
+package GPACK;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Image;
@@ -6,6 +14,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 import java.awt.Color;
 import java.awt.Font;
 
@@ -13,9 +23,13 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener {
-
+	private String imgString;
+	private int berriesNum;
+	private boolean[] b = {false, false, false, false};
 	private GenieTeams s;
-	
+	private File file;
+	private AudioInputStream audiostream;
+	private Clip clip;
 	private PersonalGenie s1;
 	
 	private PersonalGenie panel;
@@ -61,13 +75,22 @@ public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener
 	private JLabel lblNewLabel_8;
 	private JLabel lblNewLabel_9;
 	
-	public GenieRunnerL2() {
-		
+	public GenieRunnerL2(int berries, boolean[] bought, String Bimg) {
+		imgString = Bimg;
+		b = bought;
+		berriesNum = berries;
 		JFrame f = new JFrame();
 		
 		f.getContentPane().setLayout(null);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLocationRelativeTo(null);
+		
+		try {
+			playGameMusic();
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 		
 		panel_14 = new JPanel();
 		panel_14.setBounds(513, 230, 287, 39);
@@ -142,8 +165,8 @@ public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener
 		panel_8.setBounds(0, 0, 182, 18);
 		panel_4.add(panel_8);
 		
-		Image img1 = new ImageIcon(this.getClass().getResource("demonking10.png")).getImage();
-		lblNewLabel_5.setIcon(new ImageIcon(img1));
+		ImageIcon img1 = new ImageIcon("demonking10.png");
+		lblNewLabel_5.setIcon(img1);
 		
 		panel_9 = new JPanel();
 		panel_9.setBackground(Color.LIGHT_GRAY);
@@ -161,42 +184,36 @@ public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener
 		s.add(lblNewLabel_1);
 		panel_17.setVisible(false);
 		
-		Image img = new ImageIcon(this.getClass().getResource("Screen_Shot_2022-06-24_at_8.41.11_AM_219x190-removebg-preview.png")).getImage();
-		lblNewLabel_1.setIcon(new ImageIcon(img));
+		ImageIcon img = new ImageIcon("Screen_Shot_2022-06-24_at_8.41.11_AM_219x190-removebg-preview.png");
+		lblNewLabel_1.setIcon(img);
 		
-		Image img2 = new ImageIcon(this.getClass().getResource("redx.png")).getImage();
+		ImageIcon img2 = new ImageIcon("redx.png");
 		
 		lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setBounds(492, 28, 219, 190);
 		s.add(lblNewLabel_2);
-		lblNewLabel_2.setIcon(new ImageIcon(img));
+		lblNewLabel_2.setIcon(img);
 		
 		lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setBounds(291, 28, 219, 190);
 		s.add(lblNewLabel_3);
-		lblNewLabel_3.setIcon(new ImageIcon(img));
+		lblNewLabel_3.setIcon(img);
 		
 		lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setBounds(44, 28, 219, 190);
 		s.add(lblNewLabel_6);
-		lblNewLabel_6.setIcon(new ImageIcon(img2));
+		lblNewLabel_6.setIcon(img2);
 		
 		lblNewLabel_7 = new JLabel("New label");
 		lblNewLabel_7.setBounds(275, 28, 219, 190);
 		s.add(lblNewLabel_7);
-		lblNewLabel_7.setIcon(new ImageIcon(img2));
+		lblNewLabel_7.setIcon(img2);
 		
 		lblNewLabel_8 = new JLabel("");
 		lblNewLabel_8.setBounds(506, 28, 219, 190);
 		s.add(lblNewLabel_8);
-		lblNewLabel_8.setIcon(new ImageIcon(img2));
+		lblNewLabel_8.setIcon(img2);
 		
-		lblNewLabel_9 = new JLabel("New label");
-		lblNewLabel_9.setBounds(0, 0, 888, 609);
-		f.getContentPane().add(lblNewLabel_9);
-		
-		Image img3 = new ImageIcon(this.getClass().getResource("sunset.png")).getImage();
-		lblNewLabel_9.setIcon(new ImageIcon(img3));
 		
 		lblNewLabel_1.setVisible(false); 
 		lblNewLabel_2.setVisible(false); 
@@ -471,12 +488,35 @@ public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener
 			public void actionPerformed(ActionEvent e) {
 				
 				if (count7 == 2 || count7 == -2) {
+	
+					f.dispose();
 					JFrame frame = new JFrame();
 					frame.getContentPane().setLayout(null);
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 					frame.setSize(240, 120);
+					JButton newButton = new JButton("Back Home");
+					newButton.setBounds(10, 20, 100, 20);
+					frame.add(newButton);
+					
+					newButton.addActionListener(new ActionListener() {
+						
+
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							clip.stop();
+							frame.dispose();
+							try {
+								StartMenu kop = new StartMenu(0, berriesNum, "Level 1", imgString, b);
+							} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							
+						}
+						
+					});
 					if (count7 == 2) {
 						JLabel label = new JLabel();
 						label.setBounds(10, 3, 240, 120);
@@ -599,8 +639,14 @@ public class GenieRunnerL2 extends JFrame implements ActionListener, KeyListener
 	}
 	public void actionPerformed(ActionEvent e) {}
 	
-	public static void main (String [] args) {
-		GenieRunnerL2 r = new GenieRunnerL2();
+	public void playGameMusic() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		file = new File("8BIT.wav");
+		audiostream =  AudioSystem.getAudioInputStream(file);
+		clip = AudioSystem.getClip();
+		clip.open(audiostream);
+		
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
 	}
+	
 	
 }
